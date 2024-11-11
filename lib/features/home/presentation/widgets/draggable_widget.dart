f9a781fe12369b9fcdd2f96c3857ae3bfd44f66a
damooola <damolanelson@gmail.com>
2024-11-11 21:36:03 +0100

    add onPan funcs with animated builder

--- lib/features/home/presentation/widgets/draggable_widget.dart
+++ lib/features/home/presentation/widgets/draggable_widget.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 enum SlideDirection { left, right }
 
 class DraggableWidget extends StatefulWidget {
-  const DraggableWidget(
-      {super.key,
-      required this.child,
-      this.onSlideOut,
-      this.onPressed,
-      required this.isEnableDrag});
+  const DraggableWidget({
+    super.key,
+    required this.child,
+    this.onSlideOut,
+    this.onPressed,
+    required this.isEnableDrag,
+  });
   final Widget child;
   final ValueChanged<SlideDirection>? onSlideOut;
   final VoidCallback? onPressed;
@@ -21,12 +22,14 @@ class DraggableWidget extends StatefulWidget {
 class _DraggableWidgetState extends State<DraggableWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController restoreController;
-
   final _widgetKey = GlobalKey();
   Offset startOffset = Offset.zero;
   Offset panOffset = Offset.zero;
   Size size = Size.zero;
   double angle = 0;
+
+  bool isSlideMade = false;
+  double get outSizeLimit => size.width * 0.65;
 
   void onPanStart(DragStartDetails details) {
     if (!restoreController.isAnimating) {
@@ -51,16 +54,44 @@ class _DraggableWidgetState extends State<DraggableWidget>
     restoreController.forward();
   }
 
+  void restoreAnimationListener() {
+    if (restoreController.isCompleted) {
+      restoreController.reset();
+      panOffset = Offset.zero;
+      setState(() {});
+    }
+  }
+
+  Offset get getCurrentPosition {
+    final renderBox =
+        _widgetKey.currentContext?.findRenderObject() as RenderBox?;
+    return renderBox?.localToGlobal(Offset.zero) ?? Offset.zero;
+  }
+
+  void getChildSize() {
+    size =
+        (_widgetKey.currentContext?.findRenderObject() as RenderBox?)?.size ??
+            Size.zero;
+  }
+
   @override
   void initState() {
     restoreController =
-        AnimationController(vsync: this, duration: kThemeAnimationDuration);
+        AnimationController(vsync: this, duration: kThemeAnimationDuration)
+          ..addListener(restoreAnimationListener);
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        getChildSize();
+      },
+    );
     super.initState();
   }
 
   @override
   void dispose() {
-    restoreController.dispose();
+    restoreController
+      ..removeListener(restoreAnimationListener)
+      ..dispose();
     super.dispose();
   }
 
@@ -68,17 +99,17 @@ class _DraggableWidgetState extends State<DraggableWidget>
   Widget build(BuildContext context) {
     final child = SizedBox(key: _widgetKey, child: widget.child);
     if (!widget.isEnableDrag) return child;
-
     return GestureDetector(
-      onPanStart: onPanStart,
-      onPanUpdate: onPanUpdate,
-      onPanEnd: onPanEnd,
-      child: AnimatedBuilder(
+        onPanStart: onPanStart,
+        onPanUpdate: onPanUpdate,
+        onPanEnd: onPanEnd,
+        child: AnimatedBuilder(
           animation: restoreController,
           builder: (context, child) {
             final value = 1 - restoreController.value;
             return Transform.translate(offset: panOffset * value, child: child);
-          }),
-    );
+          },
+          child: child,
+        ));
   }
 }
